b5cf215a610bbc01093b6a8ba69e4df3ab3dfce0
rabaa-hasan <rabaa.reyad.hasan@gmail.com>
2021-02-08 15:29:23 +0200

    editing javascript

---
+++ skincare.js
@@ -0,0 +1,29 @@
+alert(" Hello \n This is A SkinCare Page ");
+var buy = prompt("What do you want Cream or mask?");
+while (buy !== "cream" && buy !== "mask")
+{
+    var buy = prompt("What do you want Cream or mask?");
+
+}
+var itemBuy='';
+if (buy =="cream")
+{
+    itemBuy='<img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1603131224-41297.jpg?crop=0.668xw:1.00xh;0,0.00160xh&resize=768:*"/>';
+
+}
+else if(buy =="mask")
+{
+    itemBuy='<img src="https://cf.shopee.ph/file/ca9b4ebcfcec77b0bc923ac48ce7834c">';
+
+}
+var numofproducts = prompt("Hom many of the product you want?");
+var result ='';
+var re='';
+for (var i=0 ; i<numofproducts ; i++)
+{
+    result+=itemBuy;
+    re+=1;
+}
+document.write(result);
+var s ='Number Of products is';
+console.log(s);
